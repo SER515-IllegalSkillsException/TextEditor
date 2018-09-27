537b739e7234321349e8726e61cd322757e7fcbf
xiangwei <xzheng80@asu.edu>
2018-09-26 18:18:36 -0700

    merge

--- src/main/java/view/AbstractViewPanel.java
+++ src/main/java/view/AbstractViewPanel.java
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 package view;
 
 import javax.swing.*;
@@ -13,3 +14,20 @@ public abstract class AbstractViewPanel extends JPanel {
 
 
 }
+=======
+package view;
+
+import javax.swing.*;
+
+/**
+ * All common elements and functions for Panel Views go here
+ **/
+public abstract class AbstractViewPanel extends JPanel {
+	/**
+	 * Default serial version id
+	 */
+	private static final long serialVersionUID = 1L;
+
+
+}
+>>>>>>> d9fa38685eb4e78c42db42702cc4542d3156f5a7
