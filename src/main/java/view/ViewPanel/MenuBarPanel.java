1a2879f291c3cf1745a00bba39164944db20e0de
xiangweizheng <43075724+xiangweizheng@users.noreply.github.com>
2018-09-26 17:25:00 -0700

    Update MenuBarPanel.java
    US22 Edit dropdown

--- src/main/java/view/ViewPanel/MenuBarPanel.java
+++ src/main/java/view/ViewPanel/MenuBarPanel.java
@@ -1,63 +1,78 @@
 package view.ViewPanel;
 
-import javax.swing.*;
-import java.awt.*;
+import java.awt.BorderLayout;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+import javax.swing.JPanel;
+import javax.swing.KeyStroke;
+
+import constant.EditorConstants;
+import listener.FileButtonsActionsListener;
 
 public class MenuBarPanel extends JPanel {
-    JPanel menuBarPanel = new JPanel();
-    JMenuBar menuBar;
-    JMenu fileMenu;
-    
-    JMenuItem[] fileMenuItems;
-    
-    String[] fileMenuItemLabels = {"New File", "Open File", "Save File", "Save As", "Print"};
-    int fileMenuItemsNumber = 5;
-    int menuBarPanelSizeWidth = 800;
-    int menuBarPanelSizeHeight = 800;
+	/**
+	 * Default serial id
+	 */
+	private static final long serialVersionUID = 1L;
+	JPanel menuBarPanel = new JPanel();
+	JMenuBar menuBar;
+	JMenu fileMenu;
+	JMenuItem[] fileMenuItems;
 
+	int fileMenuItemsNumber = 5;
+	int menuBarPanelSizeWidth = 800;
+	int menuBarPanelSizeHeight = 800;
+    
     
     JMenu EditMenu;
     JMenuItem[] EditMenuItems;
     String[] EditMenuItemLabels = {"Cut", "Copy", "Paste", "Select all", "Find and replace"};
     int EditMenuItemsNumber = 5;
-    
-    public MenuBarPanel() {
-        this.initialize();
-    }
 
-    public JPanel getMenuBarPanel() {
-        return menuBarPanel;
-    }
+	public MenuBarPanel() {
+		this.initialize();
+	}
 
-    private void initialize() {
-        menuBarPanel.setLayout(new BorderLayout());
-        this.createMenuBar();
-        menuBarPanel.add(menuBar, BorderLayout.NORTH);
-        menuBarPanel.setSize(menuBarPanelSizeWidth, menuBarPanelSizeHeight);
-        menuBarPanel.setVisible(true);
-    }
+	public JPanel getMenuBarPanel() {
+		return menuBarPanel;
+	}
 
-    private void createMenuBar() {
-        menuBar = new JMenuBar();
-        this.setFileMenu();
-        menuBar.add(fileMenu);
+	private void initialize() {
+		menuBarPanel.setLayout(new BorderLayout());
+		this.createMenuBar();
+		menuBarPanel.add(menuBar, BorderLayout.NORTH);
+		menuBarPanel.setSize(menuBarPanelSizeWidth, menuBarPanelSizeHeight);
+		menuBarPanel.setVisible(true);
+	}
+
+	private void createMenuBar() {
+		menuBar = new JMenuBar();
+		this.setFileMenu();
+		menuBar.add(fileMenu);
         menuBar.add(EditMenu);
-    }
+	}
 
+	private void setFileMenu() {
+		fileMenu = new JMenu(EditorConstants.FILE_MENU_TITLE);
+		fileMenuItems = new JMenuItem[fileMenuItemsNumber];
 
-    private void setFileMenu(){
-        fileMenu = new JMenu("File");
-        fileMenuItems = new JMenuItem[fileMenuItemsNumber];
+		for (int i = 0; i < fileMenuItemsNumber; i++) {
+			fileMenuItems[i] = new JMenuItem(EditorConstants.fileMenuItemLabels[i]);
+			ActionListener actionListener = new FileButtonsActionsListener(EditorConstants.fileMenuItemLabels[i]);
+			// Adding shortcut keys for the file menu
+			KeyStroke keyStroke = KeyStroke.getKeyStroke(EditorConstants.fileMenuItemsShortcuts[i],
+					KeyEvent.CTRL_DOWN_MASK);
+			fileMenuItems[i].setAccelerator(keyStroke);
+			fileMenuItems[i].addActionListener(actionListener);
+		}
 
-        for (int i = 0; i < fileMenuItemsNumber; i++) {
-            fileMenuItems[i] = new JMenuItem(fileMenuItemLabels[i]);
-        }
-        
-        for (int i = 0; i < fileMenuItemsNumber; i++) {
-            fileMenu.add(fileMenuItems[i]);
-        }
-        
-        
+		for (int i = 0; i < fileMenuItemsNumber; i++) {
+			fileMenu.add(fileMenuItems[i]);
+
         
         EditMenu = new JMenu("Edit");
         EditMenuItems = new JMenuItem[EditMenuItemsNumber];
