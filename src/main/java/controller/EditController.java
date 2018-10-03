bbaec60633521ba0b34c9c54b1527061ebdc513a
Debarati Bhattacharyya <dbhatt14@asu.edu>
2018-10-03 13:20:29 -0700

    Added functionality for "Cut"

--- src/main/java/controller/EditController.java
+++ src/main/java/controller/EditController.java
@@ -1,8 +1,5 @@
 package controller;
 
-import view.ViewPanel.TextPanel;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 
@@ -14,7 +11,6 @@ public class EditController implements ControllerInterface {
 	static JTextArea c = FileModel.getInstance().getTextArea();
 	
 	public static void cutText() {
-		System.out.println("HI I am Cut");
 		c.addMouseListener(new MouseAdapter() {
 
 		      @Override
@@ -23,25 +19,8 @@ public class EditController implements ControllerInterface {
 		      }
 		});
 		c.cut();
-		/*TextPanel textArea = new TextPanel();
-		JScrollPane t = (JScrollPane)textArea.getPanel();
-		JTextArea c = (JTextArea)t.getViewport().getView();*/
-		
-		/*String s = c.getSelectedText();
-		System.out.print(s);
-		c.cut();*/
-		
-		/*JTextArea t;
-		t = new JTextArea();
-		t.cut();*/
 		return;
 	}
 	
-	/*public void mouseReleased(MouseEvent e) {
-	    if (c.getSelectedText() != null) { // See if they selected something 
-	        String s = c.getSelectedText();
-	        System.out.print(s);
-	        // Do work with String s
-	    }
-	}*/
+	
 }
