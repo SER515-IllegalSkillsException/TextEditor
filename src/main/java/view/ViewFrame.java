bfcf16ca84ea7b22da3aaea48f974885bd919fa6
xiangwei <xzheng80@asu.edu>
2018-10-11 23:10:10 -0700

    add sepearator for user story #39 Change Text Appearance-font size #131 Change text appearance-font type

--- src/main/java/view/ViewFrame.java
+++ src/main/java/view/ViewFrame.java
@@ -5,6 +5,7 @@ package view;
 import java.awt.BorderLayout;
 import java.awt.Dimension;
 import java.awt.Font;
+import java.awt.GraphicsEnvironment;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
@@ -13,9 +14,14 @@ import javax.swing.JComboBox;
 import javax.swing.JComponent;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.JSeparator;
+import javax.swing.JTextArea;
+import javax.swing.SwingConstants;
 import javax.swing.WindowConstants;
 
 import constant.EditorConstants;
+import controller.EditController;
+import model.FileModel;
 
 /**
  * An Abstract Factory Pattern layout for the views all the View Frame has to do
@@ -40,7 +46,8 @@ public class ViewFrame extends JFrame {
 
 	private static int headPanelHeight = 75;
 	private static int headPanelWidth = 75;
-	private  JComboBox fontSize;//xiangwei
+	private  JComboBox fontSize,fontType;//xiangwei add fontSize
+
 
 	/**
 	 * Initializes all the component of the text editor and sets up the layout
@@ -57,11 +64,43 @@ public class ViewFrame extends JFrame {
 		footerPanel = PanelFactory.createPanel(EditorConstants.FOOTERPANEL);
 		headPanel.add(menuPanel,"Menu Panel");
 		headPanel.add(toolPanel,"Tool Panel");
-		headPanel.setPreferredSize(new Dimension(headPanelWidth,headPanelHeight));
-		layoutHelper();
+	
+        headPanel.add(new JSeparator(SwingConstants.VERTICAL));
+        headPanel.add(new JSeparator(SwingConstants.HORIZONTAL));
+		
+
+		
+		fontType = new JComboBox();
+
+        
+        String[] fonts = GraphicsEnvironment.getLocalGraphicsEnvironment().getAvailableFontFamilyNames();
+
+        for (int i = 0; i < fonts.length; i++) {
+    
+            fontType.addItem(fonts[i]);
+        }
+       
+        fontType.setMaximumSize(new Dimension(170, 30));
+        fontType.setToolTipText("Font Type");
+        headPanel.add(fontType);
+        
+        
+        headPanel.add(new JSeparator(SwingConstants.HORIZONTAL));
+        headPanel.add(new JSeparator(SwingConstants.VERTICAL));
+
+        //Adding Action Listener on fontType JComboBox
+        fontType.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent ev) {     
+            	String p = fontType.getSelectedItem().toString();                     
+                EditController.setfonttypeText(p);          
+            }
+        });
 		
 		
-		fontSize = new JComboBox();//xiangwei
+		
+		
+		
+		fontSize = new JComboBox();//xiangwei add fontSize
 
 
         for (int i = 5; i <= 100; i++) {
@@ -70,27 +109,30 @@ public class ViewFrame extends JFrame {
         fontSize.setMaximumSize(new Dimension(70, 30));
         fontSize.setToolTipText("Font Size");
         headPanel.add(fontSize);
+        
+        
 
+        
         fontSize.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent ev) {
-                String sizeValue = fontSize.getSelectedItem().toString();
-                int sizeOfFont = Integer.parseInt(sizeValue);
-                String fontFamily = textPanel.getFont().getFamily();
+               String sizeValue = fontSize.getSelectedItem().toString();
 
-                Font font1 = new Font(fontFamily, Font.PLAIN, sizeOfFont);
-                textPanel.setFont(font1);
+            	
+                int sizeOfFont = Integer.parseInt(sizeValue);
+                
+                EditController.setfontsizeText(sizeOfFont);   
             }
         });
 		
 		
 		
 		
+        headPanel.setPreferredSize(new Dimension(headPanelWidth,2*headPanelHeight));
 		
 		
 		
 		
-		
-		
+        layoutHelper();
 	}
 
 	/**
@@ -101,7 +143,7 @@ public class ViewFrame extends JFrame {
 		add(headPanel, BorderLayout.PAGE_START);
 		add(textPanel, BorderLayout.CENTER);
 		add(footerPanel, BorderLayout.PAGE_END);
-		setMinimumSize(new Dimension(EditorConstants.FRAME_WIDTH, EditorConstants.FRAME_HEIGHT));
+		setMinimumSize(new Dimension(2*EditorConstants.FRAME_WIDTH, 2*EditorConstants.FRAME_HEIGHT));
         setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
         setVisible(true);
 	}
