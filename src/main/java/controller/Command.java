9c3761209616d137baeb2f2fa88b4d599d7d8864
melissaDay1 <melissa.day@asu.edu>
2018-11-17 17:40:20 -0700

    US #8, Task #187 Added basic structure for undo operations using the command design pattern. Not working yet.

---
+++ src/main/java/controller/Command.java
@@ -0,0 +1,12 @@
+package controller;
+
+/**
+ * Interface for enforcing the undo/redo commands
+ * 
+ * @author Melissa
+ *
+ */
+public interface Command {
+	public void execute();
+
+}
---
+++ src/main/java/controller/RedoCommand.java
@@ -0,0 +1,16 @@
+package controller;
+
+/**
+ * Controls the redo functionality
+ * @author Melissa
+ *
+ */
+public class RedoCommand implements Command {
+
+	@Override
+	public void execute() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
