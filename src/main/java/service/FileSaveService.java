4a1f7a06f2e14b44abb35caa61d0bbd43efca069
Debarati Bhattacharyya <dbhatt14@asu.edu>
2018-11-29 17:08:49 -0700

    Fix for issue 165 : Saving file with '.txt' has issue with output

--- src/main/java/service/FileSaveService.java
+++ src/main/java/service/FileSaveService.java
@@ -1,7 +1,9 @@
 package service;
 
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileOutputStream;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.ObjectOutputStream;
 import java.nio.file.Files;
@@ -112,7 +114,11 @@ public class FileSaveService {
 	 * @throws BadLocationException
 	 */
 	private void writeText() throws IOException, BadLocationException {
-		saveObectToFile(FileModel.getInstance().getTextArea(), fileToSave);
+		//saveObectToFile(FileModel.getInstance().getTextArea(), fileToSave);
+		System.out.println("Saving Text File");
+		BufferedWriter out = new BufferedWriter(new FileWriter(fileToSave.getPath()));
+        out.write(FileModel.getInstance().getTextArea().getText());
+        out.close();
 	}
 
 	/**
